9542ace9083621851b3a27c9ed2e4d82d8a4bbba
eason <crazy653555@gmail.com>
2017-11-16 23:29:47 +0800

    建立架構

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Message } from "./message";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  content = "";
+  messages: any[] = [];
+  users = [
+    { id: 1, name: "avon" },
+    { id: 2, name: "eason" },
+    { id: 3, name: "mary" }
+  ];
+
+  user;
+
+
+
+  //加入訊息
+  addMessage() {
+    //如果有值
+    if (this.content) {
+      let newMessage: Message = {
+        message: this.content
+      };
+
+      this.messages.push(newMessage);
+      this.content = "";
+    }
+  }
+
+  selectUser($event){
+    console.log('selectUser()');
+
+    console.log($event);
+  }
 }
